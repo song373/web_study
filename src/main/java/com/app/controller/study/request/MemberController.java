756b782d0a49ddeb7dd1b2b5cf68fe8727ec63b1
song373 <song120437@naver.com>
2025-07-09 14:11:35 +0900

    Request 요청 URL 맵핑 (컨트롤러)

---
+++ src/main/java/com/app/controller/study/request/MemberController.java
@@ -0,0 +1,15 @@
+package com.app.controller.study.request;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+
+	
+	@GetMapping("/notice") //	/board/notice
+	public String notice() {
+		return "board/notice";
+	}
+}
