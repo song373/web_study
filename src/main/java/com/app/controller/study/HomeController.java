bf15b162c0907b4d23408257f7d48465760bcea1
song373 <song120437@naver.com>
2025-07-11 09:12:22 +0900

    ViewResolver 설정 부분 확인

---
+++ src/main/java/com/app/controller/study/HomeController.java
@@ -0,0 +1,19 @@
+package com.app.controller.study;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+	public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+
+	//return "/WEB-INF/views/home.jsp";
+	//return "/WEB-INF/views/mainpage.jsp";
+
+	//--ViewResolver 설정 이후
+	return "mainpage";
+	}
+	}
+
