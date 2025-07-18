3af7c6e353847eb248aade43e2f83dc4ec03e164
song373 <song120437@naver.com>
2025-07-18 12:31:03 +0900

    lombok(롬복) 설정 및 사용

---
+++ src/main/java/com/app/dto/price/RoomPrice.java
@@ -0,0 +1,8 @@
+package com.app.dto.price;
+
+import lombok.Data;
+
+@Data
+public class RoomPrice {
+
+}
--- src/main/java/com/app/dto/room/Room.java
+++ src/main/java/com/app/dto/room/Room.java
@@ -1,5 +1,29 @@
 package com.app.dto.room;
 
-public class Room {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+//@Setter
+//@Getter
+//@ToString
+//@NoArgsConstructor 
+//@AllArgsConstructor
+
+@Data
+public class Room {
+	
+	int roomId; //PK 기본키 식별자
+	String bulidingNumber;
+	int roomNumber; //호실번호
+	int floor; //층수
+	int maxGuestCount; //최대 숙박 인원
+	String viewType; //뷰 구분 코드
+					//오션뷰 시티뷰 마운틴뷰
+					// OCN CTY MOT
+					// 1	2	3
+	
 }
