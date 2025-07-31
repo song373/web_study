0662f7e846615e501b941cd46d8532d440d9b2c8
song373 <song120437@naver.com>
2025-07-31 10:44:35 +0900

    유효성 검증 2 (DTO 어노테이션 기준)

--- src/main/java/com/app/dto/user/User.java
+++ src/main/java/com/app/dto/user/User.java
@@ -1,15 +1,22 @@
 package com.app.dto.user;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class User {
 	
+	//필수입력 빈칸X
+	@NotBlank(message = "이거 아이디 필순데?")
 	String id;
+	
+	//입력 길이 제한
+	@Size(min = 8, max = 12, message = "비번 길이 확인하라고")
 	String pw;
 	String name;
 	String userType; // CUS / ADM
 					 // 사용자 계정 구분
 					 // Customer / Admin
-
 }
